acbe6c22ef4498cd1cb48cd4ea5a11ee231bd9d9
Carl Wilson <carl@openpreservation.org>
2024-04-23 17:48:00 +0100

    FEAT: New site publication - specifcation structure file `specification.yaml`; - new scripts for publisher; and - removed old scripts and collatoral.

---
+++ create_site.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+echo "Generating GitHub pages site from markdown"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$SCRIPT_DIR" || exit-site
+
+echo " - Cleaning up site directory and copying spec-publisher site..."
+git clean -f site/ specification/
+cp -rf spec-publisher/site/* site/
+cp specification/postface/postface.md doc/site/
+echo " - Generating main site specification and PDF markdown..."
+
+mvn clean package -f spec-publisher/pom.xml
+java -jar ./spec-publisher/target/mets-profile-processor-0.1.0-SNAPSHOT.jar -f ./specification.yaml -o doc/site profile/E-ARK-CSIP.xml
+
+echo " - MARKDOWN-PP: generating site page with TOC..."
+cd doc/site || exit
+bash "$SCRIPT_DIR/spec-publisher/scripts/create-venv.sh"
+command -v markdown-pp >/dev/null 2>&1 || {
+  tmpdir=$(dirname "$(mktemp -u)")
+  source "$tmpdir/.venv-markdown/bin/activate"
+}
+markdown-pp body.md -o body_toc.md
+
+echo " - MARKDOWN-PP: generating site index.md..."
+markdown-pp SITE.md -o "$SCRIPT_DIR"/site/index.md
+
+cd "$SCRIPT_DIR" || exit
+
+echo " - copying files to site directory..."
+cp -rf spec-publisher/res/md/figs site/
+# Copy remaining collaterel
+cp -rf profile archive examples specification/figs site/
+
+if [ -d _site ]
+then
+  echo " - Removing old site directory"
+  rm -rf _site
+fi
---
+++ doc/site/SITE.md
@@ -0,0 +1,19 @@
+---
+!INCLUDE "../../metadata.yaml"
+---
+
+# {{ page.subtitle }}
+
+!INCLUDE "preface.md"
+
+# {{ page.title }}
+
+## {{ page.subtitle }}
+
+Version: {{ page.version }}
+
+Date: {{ page.date }}
+
+!INCLUDE "body_toc.md"
+
+!INCLUDE "postface.md"
